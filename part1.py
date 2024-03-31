0311b546646da30d7ebfcfafbf5e0e464c43c2e6
Karasik777 <36056891+Karasik777@users.noreply.github.com>
2024-03-31 13:22:08 +1000

    Update

--- part1.py
+++ part1.py
@@ -10,35 +10,28 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-#for cancer dataset
-#dataset = pd.read_csv('dataset.csv')
-# x = dataset.drop(columns=["diagnosis(1=m, 0=b)"]) #Column of file
-# y = dataset["diagnosis(1=m, 0=b)"] 
-
-#for primes dataset:
-# dataset = pd.read_csv('primenumbers.csv')
-# x = dataset.drop(columns=["2"])
-# y = dataset["2"]
-
 #Try testing the error of AI
 def is_prime(num):
-    flag = False
-    if num > 1:
-        for i in range(2,num):
-            if num % i == 0:
-                flag = True
-                break
-
-        return flag
+    if num <=  1:
+        return False
+    if num == 2: 
+        return True
+    if num % 2 == 0:
+        return False 
+    max_devisor = int(np.sqrt(num))
+    for i in range(3, max_devisor + 1, 2):
+        if num % i == 0:
+            return False
+    return True
 
 #for generated primes:
-numbers = np.arange(1, 10001)
-labels = np.array([1 if is_prime(num) else 0 for num in numbers])
+# numbers = np.arange(1, 10001)
+# labels = np.array([1 if is_prime(num) else 0 for num in numbers])
 
 #for document reading 
-# document = pd.read_csv('primenumbers.csv')
-# numbers = document['2'].values
-# labels = np.array([1 if is_prime(num) else 0 for num in numbers])
+document = pd.read_csv('primenumbers.csv')
+numbers = document['Primes'].values
+labels = np.array([1 if is_prime(num) else 0 for num in numbers])
 
 #Need to filter data here like statistics
 #Primes is prediction 
@@ -48,31 +41,38 @@ labels = np.array([1 if is_prime(num) else 0 for num in numbers])
 model = tf.keras.models.Sequential()
 
 #data shape
-model.add(tf.keras.Input(shape=1,)) 
+model.add(tf.keras.Input(shape=1)) 
 
-#Adding layers
-model.add(tf.keras.layers.Dense( 256, activation='relu'))
-model.add(tf.keras.layers.Dense( 128, activation='relu'))
-model.add(tf.keras.layers.Dense( 64, activation='relu'))
+#ReLu = Linear regression model 
+for i in range(0, 8):
+    model.add(tf.keras.layers.Dense( 2 ** (i), activation='relu'))
+
+#Tanh - -1 to 1 
+for i in range(0,4):
+    model.add(tf.keras.layers.Dense( 2 ** (i), activation='tanh'))
+
+#Binary 0 and 1
 model.add(tf.keras.layers.Dense( 1, activation='sigmoid'))
-
 
 #Reasonable error due to wrong generation
 #need to set up machine learning for prediction instead of training
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) #Very interesting result
-model.fit(numbers, labels, epochs=50, validation_split=0.2)
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) 
+model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy']) 
+
+class PrintSequenceCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch % 1 == 0:  # Adjust the frequency of printing as needed
+            predictions = np.round(self.model.predict(numbers)).flatten()
+            print(f"Epoch {epoch+1} Predictions: {numbers[predictions == 1].flatten()}")
+            print(f"labels: {labels}")
+
+#Train model
+model.fit(numbers, labels, epochs=10, validation_split=0.2, callbacks=[PrintSequenceCallback()])
 
 #Compares AI prediction and actual data (Or something)
-#model.evaluate(x_test, y_test)
-
 test_loss, test_acc = model.evaluate(numbers, labels)
+
+#Just testing parameters
 print('Test Accuracy:', test_acc)
-
-
-
-
-
-
-
-
-
+print(f"{numbers}")
+print(f"{labels}")
